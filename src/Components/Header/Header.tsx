9c554be110d0cb758e834c12d672c566ca525154
amel <amelstutic@gmail.com>
2024-03-14 15:00:56 +0100

    Add Logout handling

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,15 +1,40 @@
 import "./header.css";
+import { useEffect,useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Provera da li postoji token u localStorage
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token); // Ako token postoji, postavljamo isLoggedIn na true
+  }, []);
+
+  const handleLogout = () => {
+    // Brisanje tokena iz localStorage-a prilikom odjave
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+  };
   return (
     <div id="headerMain">
       <img className="logoImg" src="./globusLogo.png" alt="logo" />
-      <div className="logIn-LogOutDiv">
-        <a href="/logIn">Log in</a>
-        <p>/</p>
-        <a href="/">Register</a>
+        {isLoggedIn ? (
+          // Ako je korisnik ulogovan, prikaži Logout link
+          <div className="logIn-LogOutDiv">
+          <a href="/" onClick={handleLogout}>Logout</a>
+          </div>
+        ) : (
+          // Ako korisnik nije ulogovan, prikaži Login i Register linkove
+          <div className="logIn-LogOutDiv">
+            <Link to="/logIn">Login</Link>
+            <p>/</p>
+            <Link to="/">Register</Link>
+          </div>
+        )}
       </div>
-    </div>
+     
+    
   );
 };
 
